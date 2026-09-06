e1deeb2eb7b895a9c844b1a0f945887189a53af2
ArielCatucuamba <davidcatudiaz22@gmail.com>
2026-09-06 18:52:49 -0500

    fix: deploy en el servidor

--- Backend/src/routers/User_routes.js
+++ Backend/src/routers/User_routes.js
@@ -8,7 +8,7 @@ import {
   obtenerUsuario, 
   actualizarUsuario, 
   eliminarUsuario 
-} from '../controllers/User_controllers.js';
+} from '../controllers/user_controllers.js';
 import { verificarTokenJWT } from '../middlewares/JWT.js';
 
 const router = express.Router();
